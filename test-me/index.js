35a0ad12895d3ba0447866e62a77c8be35e0cb6b
Max <jmlong.dev@gmail.com>
2022-06-15 13:47:38 -0400

    added starting files

---
+++ test-me/index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const Runner = require("./runner");
+const runner = new Runner();
+
+//helper funciton
+const run = async () => {
+  await runner.collectFiles(process.cwd());
+  console.log(runner.testFiles);
+};
+
+run();
