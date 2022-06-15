787406429af85eab9c48b879d3dae0a26393d797
Max <jmlong.dev@gmail.com>
2022-06-15 16:23:31 -0400

    testing framework

---
+++ test-me/samplewebproject/index.js
@@ -0,0 +1,10 @@
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const { value } = document.querySelector("input");
+  const header = document.querySelector("h1");
+  if (value.includes("@")) {
+    header.innerHTML = "Looks Good";
+  } else {
+    header.innerHTML = "Invalid email";
+  }
+});
